98934e80a2dca209950a261ec3ae3dfb6504a29f
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-08-07 13:27:24 +0000

    basic auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,7 +12,30 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
 
+if "AUTH_TYPE" in os.environ:
+    auth = os.environ["AUTH_TYPE"]
+    if auth:
+        from api.v1.auth.auth import Auth
+        auth = Auth()
+
+def before_request():
+    if auth is None:
+        return
+
+    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+
+    if request.path in excluded_paths:
+        return
+
+    if auth.require_auth(request):
+        if auth.authorization_header(request) is None:
+            abort(401)
+
+    if auth.current_user(request) is None:
+        abort(403)
+    
 
 @app.errorhandler(404)
 def not_found(error) -> str:
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -3,7 +3,7 @@
 creating a class to manage the api auth
 """
 from flask import request
-
+from typing import TypeVar, List
 
 
 class Auth():
@@ -12,12 +12,24 @@ class Auth():
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """handles the paths
         """
-        return False
+        if path:
+            if path[-1:] != "/":
+                path += "/"
+
+        if path is None or path not in excluded_paths or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path in excluded_paths:
+            return False
 
     def authorization_header(self, request=None) -> str:
         """the auth header method
         """
-        return None
+        if request is None:
+            return None
+        if not request.headers['Authorization']:
+            return None
+        else:
+            return request.headers['Authorization']
 
     def current_user(self, request=None) -> TypeVar('User'):
         """the current user method
