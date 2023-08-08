b8119c5a96c98b6699956cdf301b4d9c94d888ac
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-08-08 14:11:10 +0000

    user object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,8 @@
 """
 empty class that inherits from auth
 """
+from typing import TypeVar
+from models.user import User
 import base64
 
 
@@ -70,3 +72,24 @@ class BasicAuth(Auth):
 
         email, passw = decoded.split(":")
         return (email, passw)
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """returns the user instance based on his emaland pass
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        users = User.search({"email": user_email})
+
+        if not users:
+            return None
+
+        user = users[0]
+
+        if not user.is_valid_password(user_pwd):
+            return None
+
+        return user
