0d69caaab53027741b03d9ea9e797e7be32eeede
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-08-09 11:30:48 +0000

    creating a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 """
 creating a new authentication mechanism
 """
+import uuid
 
 
 from api.v1.auth.auth import Auth
@@ -10,4 +11,17 @@ from api.v1.auth.auth import Auth
 class SessionAuth(Auth):
     """creating a new auth mechanism
     """
-    pass
+    user_id_by_session_id = {}
+    session_user = user_id_by_session_id
+
+    def create_session(self, user_id: str = None) -> str:
+        """creates a session id for user_id
+        """
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid.uuid4())
+        self.session_user[session_id] = user_id
+        return session_id
