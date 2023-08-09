5cf1669b18b2cfa611ec356b5335871399f0473a
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-08-09 11:06:55 +0000

    Empy session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+creating a new authentication mechanism
+"""
+
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """creating a new auth mechanism
+    """
+    pass
