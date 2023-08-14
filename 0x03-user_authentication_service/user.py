7de014c922809ac91d77f607d06d9336887152f4
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-08-14 08:31:39 +0000

    create user

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -22,6 +22,7 @@ class User(Base):
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
 
-    def __repr__(self):
-        return "<User(email='%s', hashed_password='%s', session_id='%s', reset_token='%s')>" % (
+    def __repr__(self) -> str:
+        """string reporesentation of the above"""
+        return "<User(email='%s', hashed_password='%s',session_id='%s', reset_token='%s')>" % (
             self.email, self.hashed_password, self.session_id, self.reset_token)
