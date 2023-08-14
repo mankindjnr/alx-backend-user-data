51898a6fc1b2b7e757c447b00a940871f7937919
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-08-14 14:34:36 +0000

    destroy session task 13

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -69,9 +69,18 @@ class Auth:
         """returns the corresponding user or none"""
         try:
             user = self._db.find_user_by(session_id=session_id)
-            if user is None or session_id is None:
+            if not user or session_id is None:
                 return None
 
             return user
+        except NoResultFound:
+            return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """method updates the corresponding user is to None"""
+        try:
+            user = self._db.find_user_by(user_id=user_id)
+            user.session_id = None
+            return None
         except NoResultFound:
             return None
