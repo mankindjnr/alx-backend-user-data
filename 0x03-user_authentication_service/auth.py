81b24d47afcc5c7b39857e82ab0268f6b5fc548d
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-08-14 11:29:49 +0000

    register user task 05

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,6 +2,10 @@
 """
 hashing passwords
 """
+from db import DB
+from user import User
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 import bcrypt
 
 
@@ -11,3 +15,21 @@ def _hash_password(password: str) -> bytes:
 
     hashed_pwd = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_pwd
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """returns a user object"""
+        if self._db._session.query(User).filter_by(email=email).first():
+            raise ValueError(f"User {email} already exists")
+        _passwd = _hash_password(password)
+
+        user = User(email=email, hashed_password=_passwd)
+        self._db.add_user(email, _passwd)
+        return user
