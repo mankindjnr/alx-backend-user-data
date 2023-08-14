23e8bf33f77c624ee2a46024fbb07150a8abdb43
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-08-14 09:52:57 +0000

    hash password task 04

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -2,32 +2,6 @@
 """
 Main file
 """
-from db import DB
-from user import User
-import logging
+from auth import _hash_password
 
-
-from sqlalchemy.exc import InvalidRequestError
-from sqlalchemy.orm.exc import NoResultFound
-
-logging.getLogger('sqlalchemy.engine').setLevel(logging.WARNING)
-# Set the desired logging level for other SQLAlchemy categories
-logging.getLogger('sqlalchemy.pool').setLevel(logging.WARNING)
-logging.getLogger('sqlalchemy.dialects').setLevel(logging.INFO)
-# Add more category loggers and set their levels as needed
-
-
-my_db = DB()
-
-email = 'test@test.com'
-hashed_password = "hashedPwd"
-
-user = my_db.add_user(email, hashed_password)
-print(user.id)
-
-try:
-    my_db.update_user(user.id, hashed_password='NewPwd')
-    print("Password updated")
-except ValueError:
-    print("Error")
-
+print(_hash_password("Hello Holberton"))
