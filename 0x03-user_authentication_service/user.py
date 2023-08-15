620ca42e8c89408d01bbd84a9e185a762ab0115d
mankindjnr <kamos@stu.kemu.ac.ke>
2023-08-15 15:14:22 +0300

    user models fix-py 00

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -24,5 +24,7 @@ class User(Base):
 
     def __repr__(self) -> str:
         """string reporesentation of the above"""
-        return "<User(email='%s', hashed_password='%s',session_id='%s', reset_token='%s')>" % (
-            self.email, self.hashed_password, self.session_id, self.reset_token)
+        return "<User(email='%s',\
+            hashed_password='%s',session_id='%s', reset_token='%s')>" % (
+            self.email, self.hashed_password, self.session_id,
+            self.reset_token)
